68ffce3424c514022ec32afbe8909cd94a423b85
Ahmad Girach <ahmedkhatri6@gmail.com>
2021-09-19 16:19:05 +0530

    [FIX] pagination

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -10,9 +10,20 @@ export class PaginationComponent implements OnInit {
   @Input() records: any[] = [];
   @Output() onChange = new EventEmitter();
 
+  NO_OF_RECORDS_PER_PAGE: number = 10;
+
+  pagination: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.calculate();
+  }
+
+  calculate() {
+    const totalRecords = this.records?.length;
+    const remainder = totalRecords % this.NO_OF_RECORDS_PER_PAGE;
+    this.pagination = remainder !== 0 ? totalRecords + 1 : totalRecords;
   }
 
   handleClick(event: any) {
